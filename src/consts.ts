e3c8fb5721533006baa680f146021ef4cc1e8c9f
Diego Carlino <carlid.dev@gmail.com>
2026-07-15 14:09:51 +0200

    Add LinkedIn to sidebar socials and remove unused components.
    Order links as GitHub, X, LinkedIn; align icons with the name; drop dead Header/Footer components.

--- src/consts.ts
+++ src/consts.ts
@@ -6,13 +6,18 @@ export const SITE_DESCRIPTION = 'Welcome to my website!';
 
 export const SOCIAL_LINKS = [
 	{
+		label: 'GH',
+		href: 'https://github.com/devteapot',
+		srOnly: 'View Diego Carlino on GitHub',
+	},
+	{
 		label: 'X',
 		href: 'https://x.com/_carlid_dev',
 		srOnly: 'Follow Diego Carlino on X',
 	},
 	{
-		label: 'GH',
-		href: 'https://github.com/devteapot',
-		srOnly: 'View Diego Carlino on GitHub',
+		label: 'IN',
+		href: 'https://www.linkedin.com/in/diego-carlino/',
+		srOnly: 'View Diego Carlino on LinkedIn',
 	},
 ] as const;
